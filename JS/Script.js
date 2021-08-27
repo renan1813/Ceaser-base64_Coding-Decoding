3dfec431836b2d8ce4507a38c8fdd8398c139011
renan1813 <renanmartins1813@gmail.com>
2021-08-26 22:25:19 -0500

    commit enquanto o JS ta funcionando minimamente

--- JS/Script.js
+++ JS/Script.js
@@ -1,21 +1,27 @@
 let resultado = document.getElementById("saida");
 
-document.getElementById('texto').addEventListener("change", (el) => {
-  let entrada = el.target.value;
-  const chave = document.getElementById('chave').value;
+document.getElementById('coding').addEventListener("click", () => {
+  let entrada = document.getElementById("texto").value;
+  let chave = document.getElementById("chave").value;
+  chave = parseInt(chave);
   let arrayEntrada = entrada.split('');
   let aux1 = [];
+  let aux2 = chave;
+  let aux3 = 0;
 
   for(let i = 0; i < arrayEntrada.length; i++){
-    aux1.push(arrayEntrada[i])
+    aux3 = arrayEntrada.[i]
+    if(arrayEntrada[i].charCodeAt() >= 65 && arrayEntrada[i].charCodeAt() <= 90){
+      aux1.push(arrayEntrada[i].charCodeAt()+chave)
+    }
+    else if(arrayEntrada[i].charCodeAt() >= 96 && arrayEntrada[i].charCodeAt() <= 122){
+      aux1.push(arrayEntrada[i].charCodeAt()+chave)
+    }
   }
-  console.log(aux1)
+
+  for(let i = 0; i < aux1.length; i++){
+    aux1[i] = String.fromCharCode(aux1[i]);
+  }
+
   resultado.innerText = aux1
 })
-
-
-// if(arrayEntrada[i].charCodeAt() <= 65 && arrayEntrada.charCodeAt() >= 90){
-//   let str = arrayEntrada.charCodeAt()
-//   aux1.push(str);
-//   console.log(str)
-// }
